7a06e8405d3fe0aedd60433a9359e02223f5717a
khlifi fares <khfares261@gmail.com>
2024-04-10 06:19:58 +0200

    commit 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
-const routes:Routes=[
-  {
-    path:'',
-    component:LoginComponent
-  }
-];
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+ 
   providers: [],
   bootstrap: [AppComponent]
 })
